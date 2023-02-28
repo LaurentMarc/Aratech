8e1b5b4a3a855118bcc40a7dc6429354fa208a7d
Nico-est-la <nicolas220179@gmail.com>
2023-02-28 16:49:48 +0100

    Page expertise ok

--- app0_base/urls.py
+++ app0_base/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('team', views.team, name='team'),
     path('expertise', views.expertise, name='expertise'),
     path('ai', views.ai, name='our ai'),
